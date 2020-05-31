a1f19574e8ae9999b1e489a915884e1f8b05a214
Svetozar <svetozariliev13@gmail.com>
2020-05-31 17:49:00 +0300

    BaseButton - removed a lot of props to be lightweight

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,34 +1,20 @@
 <template>
   <button
-    @click="onClick ? onClick() : null"
     :type="type"
     :classes="classes"
     :disabled="disabled"
-    :autofocus="autofocus"
-    :form="formId"
-    :formactionUrl="formactionUrl"
-    :formenctype="formenctype"
-    :formmethod="formmethod"
-    :formnovalidate="formnovalidate"
-    :formtarget="formtarget"
     :name="name"
     :value="value"
     class="btn"
-  >{{text}}</button>
+  ><slot></slot></button>
 </template>
 
 <script>
+// How to use it:
+// <BaseButton>{{'BUTTON'}}</BaseButton>
 export default {
   name: 'BaseButton',
   props: {
-    text: {
-      type: String,
-      default: "BUTTON",
-      validator: (val) => (/^[A-Za-z0-9]{1,30}$/g).test(val)
-    },
-    onClick: {
-      type: Function,
-    },
     type: {
       type: String,
       default: "button",
@@ -37,16 +23,6 @@ export default {
     classes: {
       type: String,
     },
-    disabled: {
-      type: Boolean,
-      default: false,
-      validator: (val) => (/^false$|^true$/g).test(val)
-    },
-    autofocus: {
-      type: Boolean,
-      default: false,
-      validator: (val) => (/^false$|^true$/g).test(val)
-    },
     name: {
       type: String,
       validator: (val) => (/^[A-Za-z0-9]{1,30}$/g).test(val)
@@ -54,35 +30,6 @@ export default {
     value: {
       type: String,
       validator: (val) => (/^[A-Za-z0-9]{1,30}$/g).test(val)
-    },
-    // If the button belongs to a <form> use it's ID
-    formId: {
-      type: String,
-      validator: (val) => (/^[A-Za-z0-9]{1,30}$/g).test(val)
-    },
-    // where to send the form-data
-    formactionUrl: {
-      type: String,
-    },
-    // how form-data should be encoded
-    formenctype: {
-      type: String,
-      validator: (val) => (/^application\/x-www-form-urlencoded$|^multipart\/form-data$|^text\/plain$/g).test(val)
-    },
-    formmethod: {
-      type: String,
-      validator: (val) => (/^get$|^post$/g).test(val)
-    },
-    // form-data should NOT be validated on submission
-    formnovalidate: {
-      type: Boolean,
-      default: false,
-      validator: (val) => (/^false$|^true$/g).test(val)
-    },
-    // where to display the response after submitting the form(_blank)
-    formtarget: {
-      type: String,
-      validator: (val) => (/^_blank$|^_self$|^_parent$|^_top$|^framename$/g).test(val)
     },
   }
 }
